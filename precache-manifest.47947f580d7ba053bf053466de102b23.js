ac7592bbb7a2f7d6983082e83e246d4847ea45eb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-06 10:31:03 +0000

    Deploy BuiKiem/client to github.com/BuiKiem/client.git:gh-pages

---
+++ precache-manifest.47947f580d7ba053bf053466de102b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ef97e909c9afd991d7247e79070666",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "2395d4c242d6e9cdb866",
+    "url": "/client/static/css/main.ee28cb5a.chunk.css"
+  },
+  {
+    "revision": "9c26d44c926e8bb0d0d9",
+    "url": "/client/static/js/2.d8983aff.chunk.js"
+  },
+  {
+    "revision": "2395d4c242d6e9cdb866",
+    "url": "/client/static/js/main.1544f398.chunk.js"
+  },
+  {
+    "revision": "18971c2c783173e14063",
+    "url": "/client/static/js/runtime~main.6bddd995.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/client/static/media/logo.5d5d9eef.svg"
+  }
+]);
