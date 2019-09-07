e1a2d95d965d34388eb4d95152605db6ceeb71fb
BuiKiem <buivantrung1105@gmail.com>
2019-09-07 16:03:28 +0700

    #6 - Create Base Page UI (#17)
    * Install react-router-dom and add HomePage route.
    
    * Add React Router to App.test.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
+
+import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 if (module.hot) {
   module.hot.accept();
