c44d4f1bfd2b8fde1b725bcdd2f9709832e4459a
Bui Kiem <buivantrung1105@gmail.com>
2019-09-07 15:43:17 +0700

    Install react-router-dom and add HomePage route.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { CssBaseline } from "@material-ui/core";
+import { Switch, Route } from "react-router-dom";
 
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
+
+import { HomePage } from "./pages/HomePage";
+
 import { FloatingActionButtonZoom } from "./components/FloatingContactButton/FloatingContactButton";
 
 function App() {
@@ -10,6 +14,10 @@ function App() {
     <>
       <CssBaseline />
       <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+
       <FloatingActionButtonZoom />
       <Footer />
     </>
