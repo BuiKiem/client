00d03124e356f046e46c944cf97361049aca3f31
buikiem <buivantrung1105@gmail.com>
2019-09-09 18:19:53 +0700

    [HotelSearchForm] Add date picker input

--- src/components/SearchBox/SearchHotelForm/SearchHotelForm.js
+++ src/components/SearchBox/SearchHotelForm/SearchHotelForm.js
@@ -1,5 +1,11 @@
+import "moment";
 import React from "react";
 import Select from "react-select";
+import MomentUtils from "@date-io/moment";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker
+} from "@material-ui/pickers";
 
 const options = [
   { value: "chocolate", label: "Chocolate" },
@@ -7,12 +13,35 @@ const options = [
   { value: "vanilla", label: "Vanilla" }
 ];
 
-export const SearchHotelForm = () => (
-  <form>
-    <Select
-      id="component-destination"
-      options={options}
-      placeholder="City, hotel, place to go"
-    />
-  </form>
-);
+export const SearchHotelForm = () => {
+  const [selectedDate, setSelectedDate] = React.useState(
+    new Date("2014-08-18T21:11:54")
+  );
+
+  function handleDateChange(date) {
+    setSelectedDate(date);
+  }
+
+  return (
+    <form>
+      <Select
+        id="component-destination"
+        options={options}
+        placeholder="City, hotel, place to go"
+      />
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <KeyboardDatePicker
+          disablePast
+          disableToolbar
+          variant="inline"
+          format="DD/MM/YYYY"
+          margin="normal"
+          value={selectedDate}
+          onChange={handleDateChange}
+          id="date-picker-inline"
+          label="Check-in"
+        />
+      </MuiPickersUtilsProvider>
+    </form>
+  );
+};
